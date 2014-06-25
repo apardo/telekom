6631a8fa36fed3cbcd642327e9d1e7cded904774
Antonio Pardo <apardo@alabs.org>
2014-06-25 14:50:02 -0500

    end config

--- telekom/settings.py
+++ telekom/settings.py
@@ -70,9 +70,11 @@ DATABASES = {
 # Internationalization
 # https://docs.djangoproject.com/en/1.6/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+#LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'es'
 
-TIME_ZONE = 'UTC'
+#TIME_ZONE = 'UTC'
+TIME_ZONE = 'America/Guayaquil'
 
 USE_I18N = True
 
