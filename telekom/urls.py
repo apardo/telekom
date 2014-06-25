587d56e9654dc1fff594c7f2ad42b6d6fe2fa6e6
Antonio Pardo <apardo@alabs.org>
2014-06-25 13:30:33 -0500

    proto api

--- telekom/urls.py
+++ telekom/urls.py
@@ -1,12 +1,28 @@
 from django.conf.urls import patterns, include, url
-
 from django.contrib import admin
+from django.contrib.auth.models import User, Group
+from rest_framework import viewsets, routers
+
 admin.autodiscover()
+
+# ViewSets define the view behavior.
+class UserViewSet(viewsets.ModelViewSet):
+    model = User
+
+class GroupViewSet(viewsets.ModelViewSet):
+    model = Group
+
+# Routers provide an easy way of automatically determining the URL conf.
+router = routers.DefaultRouter()
+router.register(r'users', UserViewSet)
+router.register(r'groups', GroupViewSet)
 
 urlpatterns = patterns('',
     # Examples:
     # url(r'^$', 'telekom.views.home', name='home'),
     # url(r'^blog/', include('blog.urls')),
+
+    url(r'^', include(router.urls)),
 
     url(r'^admin/', include(admin.site.urls)),
 
