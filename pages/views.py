097201906dbf8244c3cc2f8d1ce80b043e2047fb
Antonio Pardo <apardo@alabs.org>
2014-06-25 22:06:00 -0500

    proto pages

---
+++ pages/views.py
@@ -0,0 +1,6 @@
+from django.views.decorators.http import require_http_methods
+from django.shortcuts import render_to_response
+
+@require_http_methods(["GET"])
+def index(request):
+  return render_to_response('index.html')
--- telekom/urls.py
+++ telekom/urls.py
@@ -1,8 +1,11 @@
 from django.conf.urls import patterns, include, url
-from django.contrib import admin
+
 from django.contrib.auth.models import User, Group
+from subscribers.models import Subscriber
+
 from rest_framework import viewsets, routers
 
+from django.contrib import admin
 admin.autodiscover()
 
 # ViewSets define the view behavior.
@@ -12,19 +15,22 @@ class UserViewSet(viewsets.ModelViewSet):
 class GroupViewSet(viewsets.ModelViewSet):
     model = Group
 
+class SubscriberViewSet(viewsets.ModelViewSet):
+  model = Subscriber
+
 # Routers provide an easy way of automatically determining the URL conf.
 router = routers.DefaultRouter()
 router.register(r'users', UserViewSet)
 router.register(r'groups', GroupViewSet)
+router.register(r'subscribers', SubscriberViewSet)
 
 urlpatterns = patterns('',
     # Examples:
     # url(r'^$', 'telekom.views.home', name='home'),
     # url(r'^blog/', include('blog.urls')),
 
-    url(r'^', include(router.urls)),
-
+    url(r'^$', include('pages.urls')),
+    url(r'^api/', include(router.urls)),
     url(r'^admin/', include(admin.site.urls)),
-
     url(r'^api-auth/', include('rest_framework.urls', namespace='rest_framework')),
 )
